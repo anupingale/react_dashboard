3826107e7e00b49f49ca8e39b8968ad7d6537afe
Annapurna Pingale <44020027+anupingale@users.noreply.github.com>
2019-04-17 12:30:54 +0530

    Added the dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const Users = function(props) {
+  const { users } = props;
+  console.log(users);
+  return <div>{users}</div>;
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [{ username: 'hello' }]
+    };
+    this.addUser = this.addUser.bind(this);
+    this.showUser = this.showUser.bind(this);
+  }
+
+  showUser() {
+    fetch('/getusers')
+      .then(user => user.json())
+      .then(users => {
+        this.setState({
+          users
+        });
+      });
+  }
+
+  addUser(event) {
+    const username = document.getElementById('username').value;
+    event.preventDefault();
+    fetch('/register', {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({ username })
+    })
+      .then(res => res.json())
+      .then(users => {
+        this.setState({ users });
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.users[0].username}
+        <button name="user" value="submit" onClick={this.showUser}>
+          show users
+        </button>
+        <form>
+          <input type="text" name="username" id="username" />
+          <button onClick={this.addUser}>add User</button>
+        </form>
       </div>
     );
   }
